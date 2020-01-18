bd1868db78f6a506308820ca74f43d79054c2583
Nelson AD <ndemoraes93@gmail.com>
2020-01-18 16:34:54 -0500

    january revamp

--- src/About/About.js
+++ src/About/About.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import './About.css';
 
 
 export default class About extends Component {
 
     render(){
         return(
-            <div>This is a simple app designed to calculate base income, income after federal and fica taxes, and your bonus. The next release will include State and City taxes. Future releases wll include deductions and other special filings. Made by Nelson DeMoraes.</div>
+            <div class="about">This is a simple app designed to calculate base income, income after federal and fica taxes, and your bonus. The next release will include State and City taxes. Future releases wll include deductions and other special filings.</div>
         );
     }
 
