94db25e0c1eab8714c813f5f98c112381c39067f
Nelson AD <ndemoraes93@gmail.com>
2020-01-18 15:48:18 -0500

    Updates

---
+++ precache-manifest.255c030b3fcc46dc7563badb235f62ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e3a8fda45fdfdedc9c94c4f9b02ea5",
+    "url": "/salcalc-wip/index.html"
+  },
+  {
+    "revision": "a9bc4cc809512e35ba42",
+    "url": "/salcalc-wip/static/css/main.a8dfe697.chunk.css"
+  },
+  {
+    "revision": "1f5b6910fef6e12aee45",
+    "url": "/salcalc-wip/static/js/2.af40dbbe.chunk.js"
+  },
+  {
+    "revision": "a9bc4cc809512e35ba42",
+    "url": "/salcalc-wip/static/js/main.a6f841e2.chunk.js"
+  },
+  {
+    "revision": "4090fc72563463f98bc1",
+    "url": "/salcalc-wip/static/js/runtime~main.525de802.js"
+  }
+]);
